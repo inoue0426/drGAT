ed1bb4c92745735e2a5d7d9aea44ef15470a59cd
inoue0426 <8393063+inoue0426@users.noreply.github.com>
2025-04-12 14:04:30 -0400

    add test2 for four dataset

---
+++ gdsc2_new_cell/run_tune_new_cell_MPNN.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --mem=50gb
+#SBATCH --requeue
+#SBATCH --job-name='G2 MPNN'
+#SBATCH --partition gpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=inoue019@umn.edu
+#SBATCH --time=10-00:00:00
+
+source /data/$USER/conda/etc/profile.d/conda.sh && source /data/$USER/conda/etc/profile.d/mamba.sh
+conda activate genex
+python new_cell_nci_mpnn.py
