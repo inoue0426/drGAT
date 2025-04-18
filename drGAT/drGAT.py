0f48282093d04660225d7eea605c96af7c54680b
inoue0426 <8393063+inoue0426@users.noreply.github.com>
2025-04-18 14:44:16 -0400

    change import

--- drGAT/drGAT.py
+++ drGAT/drGAT.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from sklearn.metrics import (accuracy_score, average_precision_score,
                              confusion_matrix, f1_score, precision_score,
                              recall_score, roc_auc_score)
-from torch.amp import GradScaler, autocast
+from torch.cuda.amp import GradScaler, autocast
 from torch.nn import Dropout, Linear, Module
 from torch.optim import lr_scheduler
 from torch_geometric.nn import GATConv, GATv2Conv, GraphNorm, TransformerConv
