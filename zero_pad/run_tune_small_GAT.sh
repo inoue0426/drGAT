05ab3bcbffdb46cf8051aec985bfe80a9be8dcd0
inoue0426 <8393063+inoue0426@users.noreply.github.com>
2025-04-21 10:30:11 -0500

    add zero pad results

---
+++ zero_pad/run_tune_small_GAT.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=GAT
+#SBATCH --partition=a100-4 
+#SBATCH --gres=gpu:a100:1
+#SBATCH --time=24:00:00
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=8
+#SBATCH --output=logs/%x_%j.out
+
+module load conda
+source activate /scratch.global/$USER/myenv
+python t_small_GAT.py
