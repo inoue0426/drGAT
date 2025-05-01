9745c937e8e22c4576f395eb8d881157e95fd4c4
inoue0426 <8393063+inoue0426@users.noreply.github.com>
2025-05-01 12:09:04 -0400

    change dir structure

---
+++ Test2_leave_cell_out/ctrp_new_cell/run_tune_new_cell_t.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --mem=50gb
+#SBATCH --requeue
+#SBATCH --job-name='CtrD T'
+#SBATCH --partition gpu
+#SBATCH --gres=gpu:v100x:1
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=inoue019@umn.edu
+#SBATCH --time=10-00:00:00
+
+source /data/$USER/conda/etc/profile.d/conda.sh && source /data/$USER/conda/etc/profile.d/mamba.sh
+conda activate genex
+python new_cell_nci_t.py
